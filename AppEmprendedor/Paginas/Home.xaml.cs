6d61b36cd4483843f4f85c7e8610af2e21bd5753
Carlos Fernando Roque Urbina <croqueu1907@outlook.com>
2023-09-27 20:37:25 -0600

    Revert "Revert "pagina Menu.xaml agregada""
    This reverts commit bc12fd16a5d8317be0b79e191e75c98e761c1d34.

--- AppEmprendedor/Paginas/Home.xaml.cs
+++ AppEmprendedor/Paginas/Home.xaml.cs
@@ -10,6 +10,7 @@ public partial class Home : ContentPage
     private void iconmenu_Tapped(object sender, TappedEventArgs e)
     {
 		DisplayAlert("Mensaje", "Hola", "Aceptar");
+		Shell.Current.GoToAsync(nameof(Paginas.Menu));
     }
 
 
